ad3d77c986c5aac0aad5ab5944f6e09236bedce1
farzac <zaccantte@gmail.com>
2019-07-25 23:26:44 -0300

    Add App with Login

---
+++ src/LoginPage/LoginPage0.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+
+import { userService } from '../_services';
+
+class LoginPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        userService.logout();
+
+        this.state = {
+            username: '',
+            password: '',
+            submitted: false,
+            loading: false,
+            error: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        const { name, value } = e.target;
+        this.setState({ [name]: value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        this.setState({ submitted: true });
+        const { username, password, returnUrl } = this.state;
+
+        // stop here if form is invalid
+        if (!(username && password)) {
+            return;
+        }
+
+        this.setState({ loading: true });
+        userService.login(username, password)
+            .then(
+                user => {
+                    const { from } = this.props.location.state || { from: { pathname: "/" } };
+                    this.props.history.push(from);
+                },
+                error => this.setState({ error, loading: false })
+            );
+    }
+
+    render() {
+        const { username, password, submitted, loading, error } = this.state;
+        return (
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    <Form inline>
+                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Navbar>
+
+        );
+    }
+}
+
+export { LoginPage }; 
